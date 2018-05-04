e31dd5adbce2314b7f21f6514fbf99f97f7d6ec7
USP <USP@C202-16>
2018-05-03 23:03:30 -0500

    Reporte

---
+++ Reportedekardexdenotas.php
@@ -0,0 +1,14 @@
+<html>
+    <TABLE BORDER>
+	<TR>
+		<TD>NOMBRES</TD> <TD>CURSO</TD> <TD>NOTA</TD>
+	</TR>
+	<TR>
+		<TD>FRANK MESTANZA</TD> <TD>MATEMATICA</TD> <TD>20</TD>
+	</TR>
+</TABLE>
+
+
+
+
+
