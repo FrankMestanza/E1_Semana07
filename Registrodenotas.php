5b7f597c0a0e25315848e33072bc13f8a663ff4e
USP <USP@C202-16>
2018-05-03 23:02:49 -0500

    Registro

---
+++ Registrodenotas.php
@@ -0,0 +1,23 @@
+<html>
+
+<label>Nombre</label>
+      <input type="text" name="nombre" id="name1" />
+      <br> <br/>
+      
+<label>Apellido</label>
+      <input type="text" name="apellido" id="name1" />
+        <br> <br/>
+      
+<label>Curso</label>
+      <input type="text" name="curso" id="name1" />
+      <br> <br/>
+      
+<label>Nota</label>
+      <input type="text" name="nota1" id="name1" />
+       <br> <br/>
+          
+<label>Nota 2</label>
+      <input type="text" name="nota2" id="name1" />
+      
+      <br> <br/>
+<input type="button" value="enviar">
